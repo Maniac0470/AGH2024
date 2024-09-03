7d9f9b029bd225fad02939dcd845330e6b2bfe27
Alexander Heck <Maniac47@gmail.com>
2024-09-03 14:05:36 -0400

    getting all users and their group memberships

--- ADUserCheck.ps1
+++ ADUserCheck.ps1
@@ -1,3 +1,22 @@
 ###Basic AD User Check Script###
-get-aduser -filter {name -like "*heck*"} -Properties * | Select Name, Description | Sort-Object
+# get-aduser -filter {name -like "*heck*"} -Properties * | Select Name, Description | Sort-Object
 # working through how to make Powershell work
+
+# Define the Organizational Unit (OU)
+$ou = "DC=DS,DC=local"
+
+# Get all users in the specified OU
+$users = Get-ADUser -Filter * -SearchBase $ou -Property MemberOf
+
+# Loop through each user and display their Name and Groups
+$users | ForEach-Object {
+    $user = $_
+    $groups = $user.MemberOf | ForEach-Object {
+        # Get the group name from the Distinguished Name
+        (Get-ADGroup $_).Name
+    }
+    [PSCustomObject]@{
+        Name = $user.Name
+        Groups = $groups -join "; "
+    }
+} | Select-Object Name, Groups | Format-Table -AutoSize
